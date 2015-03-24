fix some signs

advection_inverse.py
@@ -44,12 +44,12 @@ def gradient(x, u_final_data, f, psi, T, R, M):
     for i in range(1, M + 1):
         t = i * dt
         # solve adjoint problem in w
-        w = forward(x, u_final - u_final_data, -psi, np.zeros(len(x)), T - t, R)
+        w = forward(x, u_final_data - u_final, -psi, np.zeros(len(x)), T - t, R)
         # solve forward problem for u_x
         u_x = forwardGradient(x, np.zeros(len(x)), psi, f, t, R)
         # accumulate gradient
-        F_f = F_f + dt * w
-        F_psi = F_psi - np.dot(w, u_x * dx) * dt
+        F_f = F_f - dt * w
+        F_psi = F_psi - np.dot(u_x * w, dx) * dt
     
     return F_f, F_psi
 
@@ -83,9 +83,9 @@ def recoverDemo():
     R = 1.0
     N = 10000  # Nb of grid points in physical space
     M = 10  # Nb of grid points in time space
-    nb_grad_steps = 10  # Nb of updates with the gradient
+    nb_grad_steps = 100  # Nb of updates with the gradient
     alpha_f = 100000000.0  # gradient update size
-    alpha_psi = 1000000.0
+    alpha_psi = 100000.0
     
     # plots
     pp = PdfPages('./images/recover_demo.pdf')
@@ -100,11 +100,11 @@ def recoverDemo():
     u_final_data = forward(x, np.zeros(len(x)), psi_optimal, f_optimal, T, R)
 
     # initial coefficients
-    # f = np.zeros(len(x))
-    f = (
-        (1.0 / t_f) * np.exp( - (x - 0.1) * (x - 0.1) / (4.0 * t_f)) /
-        np.sqrt( 4.0 * np.pi * t_f))
-    psi = 1000
+    f = np.zeros(len(x))
+#    f = (
+#        (1.0 / t_f) * np.exp( - (x - 0.1) * (x - 0.1) / (4.0 * t_f)) /
+#        np.sqrt( 4.0 * np.pi * t_f))
+    psi = 1500
     
     # plot f
     plt.figure()    
@@ -126,14 +126,17 @@ def recoverDemo():
     print("psi_optimal : ", psi_optimal)
     print("F_psi : ", F_psi)
     
+    u_final_recovered = forward(x, np.zeros(len(x)), psi, f, T, R)    
+    
     # plot u_final_data
     plt.figure()
-    plt.plot(x, u_final_data, 'r', label="$t=T$")
+    plt.plot(x, u_final_data, 'b', label="data")
+    plt.plot(x, u_final_recovered, 'r', label="recovered")
     plt.legend(
         bbox_to_anchor=(0., 1.02, 1., .102), loc=3, 
         ncol=2, mode="expand", borderaxespad=0.)
     plt.xlabel('$x$')
-    plt.ylabel('$u$')
+    plt.ylabel('$u_{T}$')
     pp.savefig()    
     
     # plot f